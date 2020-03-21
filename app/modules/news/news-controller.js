c5f30b082c786fce2472c2ca5e3a3fe9be14d7e6
Daviid <david.hany129@gmail.com>
2020-03-21 20:13:17 +0200

    -Init the fetch article endpoint -->GET /news/

--- app/modules/news/news-controller.js
+++ app/modules/news/news-controller.js
@@ -30,6 +30,18 @@ class NewsController extends Controller {
   constructor(repository) {
     super(repository);
   }
+
+  /**
+   * Fetch all records from the repo.
+   *
+   * @param   {object}  req
+   * @param   {object}  res
+   *
+   * @return  {array}
+   */
+  async fetch(req, res) {
+    return res.json(await this.repo.fetch(req.query.pageSize, req.query.query, req.query.page, req.query.sort));
+  }
 }
 
 module.exports = NewsController;
--- app/modules/news/news-repository.js
+++ app/modules/news/news-repository.js
@@ -14,6 +14,19 @@ class NewsRepository extends Repository {
   constructor(model) {
     super(model);
   }
+
+
+  /**
+   * retrive the fetched articles form the news factory
+   * @param {Integer} pageSize
+   * @param {String} query
+   * @param {Integer} page
+   * @param {String} sort
+   * @return {Array}
+   */
+  async fetch(pageSize, query, page, sort) {
+    return container.newsfactory.getArticles(pageSize, query, page, sort);
+  }
 }
 
 module.exports = NewsRepository;
