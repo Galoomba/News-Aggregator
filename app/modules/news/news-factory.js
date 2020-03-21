ce539835a5118fd722680cbae8e4bcbd1a6eddf5
Daviid <david.hany129@gmail.com>
2020-03-22 01:36:22 +0200

    -Add caching to the news-factory -Caching ex date is 5 min

--- app/modules/news/news-factory.js
+++ app/modules/news/news-factory.js
@@ -25,16 +25,25 @@ class NewsFactory {
   async getArticles(limit = 10, query, page = 1, sort = 'desc') {
     const articlesList = [];
     const numberOfNewsFromSite = this.toFixed(limit / this.models.length, 0);
-    /**
-     * split the number of records between news model
-     */
-    for (let i = 0; i < this.models.length; i++) {
-      articlesList.push(...await this.models[i].query(numberOfNewsFromSite, query, page));
+    const cacheCompositeKey = `${limit}-${query}-${page}`;
+
+    // check if result are cached
+    const cached = await container.redis.get(cacheCompositeKey);
+    if (cached) articlesList.push(...JSON.parse(cached));
+    else {
+      /**
+       * split the number of records between news model
+       */
+      for (let i = 0; i < this.models.length; i++) {
+        articlesList.push(...await this.models[i].query(numberOfNewsFromSite, query, page));
+      }
+
+      // Make sure that the number of articles will match the user selection
+      if (limit > articlesList.length) articlesList.push(...await this.models[0].query(1, query, (limit * page) + 1));
+
+      // cache request with limit < 15 for 5 min
+      if (limit < 15) container.redis.set(cacheCompositeKey, JSON.stringify(articlesList), 'EX', 300);
     }
-
-    // Make sure that the number of articles will match the user selection
-    if (limit > articlesList.length) articlesList.push(...await this.models[0].query(1, query, (limit * page) + 1));
-
     // sort articles list by publishAt date
     return articlesList.sort((a, b) => (sort === 'desc') ? moment(b.publishedAt) - moment(a.publishedAt) : moment(a.publishedAt) - moment(b.publishedAt) );
   }
