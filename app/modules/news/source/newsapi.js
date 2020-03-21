0358180552460231fb4c94136b838dfaba1cd901
Daviid <david.hany129@gmail.com>
2020-03-21 12:30:02 +0200

    -Init reddit & newsApis classes -Add them to core provider

---
+++ app/modules/news/source/newsapi.js
@@ -0,0 +1,64 @@
+
+/**
+ * NewsApi class
+ */
+class NewsApi {
+  /**
+   * Init new object
+   *
+   * @return  {void}
+   */
+  constructor() {
+  }
+
+  /**
+   * Fetch data from reddit api
+   * @param {String} baseUrl
+   * @param {String} constrainString
+   * @return {Array}
+   */
+  static async getArticles(baseUrl, constrainString) {
+    console.log(`${baseUrl}${constrainString}`);
+    const {data} = await container.axios.get(`${baseUrl}${constrainString}`);
+    const articles = data.articles;
+    // map articles
+    return this.articleMapper(articles);
+  }
+
+  /**
+   * Maparticles
+   * @param {Array} articles
+   * @return {Array}
+   */
+  static articleMapper(articles) {
+    return articles.map( (article) =>{
+      return {
+        headline: article.title,
+        link: article.url,
+        publishedAt: container.moment(article.publishedAt).toISOString(),
+        source: 'newsapi',
+      };
+    });
+  }
+
+  /**
+   * constract conditions to Query NewsApi api and return a list of Articles
+   * @param {Integer} limit
+   * @param {String} query
+   * @param {String} page
+   * @param {String} pageSize
+   * @return {Array}
+   */
+  static query(limit = 5, query, page = 1, pageSize) {
+    // constract query string
+    let constrainString = `&apiKey=${container.config.newsApi_key}&pageSize=${limit}&page=${page}`;
+    if (query) constrainString += `&q=${query}`;
+
+    // config query base url
+    const baseUrl = (query) ? container.config.newsApi_search : container.config.newsApi_base;
+
+    return this.getArticles(baseUrl, constrainString);
+  }
+}
+
+module.exports = NewsApi;
---
+++ app/modules/news/source/reddit.js
@@ -0,0 +1,68 @@
+
+/**
+ * Reddit class
+ */
+class Reddit {
+  /**
+   * Init new object
+   *
+   * @return  {void}
+   */
+  constructor() {
+  }
+
+  /**
+   * Fetch data from reddit api
+   * @param {String} baseUrl
+   * @param {String} constrainString
+   * @return {Array}
+   */
+  static async getArticles(baseUrl, constrainString) {
+    const {data} = await container.axios.get(`${baseUrl}${constrainString}`);
+    const articles = data.data.children;
+    // map articles
+    return this.articleMapper(articles);
+  }
+
+  /**
+   * Maparticles
+   * @param {Array} articles
+   * @return {Array}
+   */
+  static articleMapper(articles) {
+    return articles.map( ({data}) =>{
+      return {
+        headline: data.title,
+        link: data.url,
+        publishedAt: container.moment.unix(data.created).toISOString(),
+        source: 'reddit',
+      };
+    });
+  }
+
+  /**
+   * constract conditions to Query reddit api and return a list of Articles
+   * @param {Integer} limit
+   * @param {String} query
+   * @param {String} page
+   * @param {String} pageSize
+   * @return {Array}
+   */
+  static query(limit = 5, query, page, pageSize) {
+    // constract query string
+    let constrainString = `&limit=${limit}&count=${limit}`;
+    if (query) constrainString += `&q=${query}`;
+    /**
+     * TODO modify that query to cache user query to map the cursor based paggination to page and page size
+     */
+    if (page) constrainString += `&after=${page}`;
+    if (page) constrainString += `&before=${page}`;
+
+    // config query base url
+    const baseUrl = (query) ? container.config.reddit_search : container.config.reddit_base;
+
+    return this.getArticles(baseUrl, constrainString);
+  }
+}
+
+module.exports = Reddit;
